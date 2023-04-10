4d5118b64fbaf9c46d5b2721bcef54818b1da4d8
Eric MARTIN <eric.martin@etud.u-picardie.fr>
2023-04-10 22:22:20 +0200

    login ok

--- projetArchiWeb/src/app/login/login.component.ts
+++ projetArchiWeb/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  nom : string = '';
+  mdp : string = '';
+  role : string = '';
+  islogin : boolean = true;
+  errorMessage : string ='';
+
+  constructor(private router: Router,private http: HttpClient)
+  {
+    
+  }
+
+  login()
+  {
+    let bodyData = {
+      nom : this.nom,
+      mdp : this.mdp,
+    };
+
+    this.http.post("http://127.0.0.1:9992/utilisateur/login",bodyData).subscribe( (resultData : any) =>
+    {
+
+      if(resultData.status) 
+      {
+        this.router.navigateByUrl('/accueil');
+      }
+      else
+      {
+        alert("nom ou mdp invalide");
+      }
+      
+    });
+
+    
+  }
 
 }
